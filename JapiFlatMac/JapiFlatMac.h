c4f24fdc30322b28e132fc0791a3224c065841ae
Saminos <syaifulamin@styletheory.co>
2020-01-07 15:17:48 +0700

    add support macOS minimum macOS 10.9 update minimum iOS versionn to iOS 10.0

---
+++ JapiFlatMac/JapiFlatMac.h
@@ -0,0 +1,19 @@
+//
+//  JapiFlatMac.h
+//  JapiFlatMac
+//
+//  Created by Saminos on 07/01/20.
+//  Copyright © 2020 Style Theory Technologies. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JapiFlatMac.
+FOUNDATION_EXPORT double JapiFlatMacVersionNumber;
+
+//! Project version string for JapiFlatMac.
+FOUNDATION_EXPORT const unsigned char JapiFlatMacVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JapiFlatMac/PublicHeader.h>
+
+
